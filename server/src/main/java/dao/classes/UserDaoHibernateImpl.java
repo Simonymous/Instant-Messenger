25c0285afbfb629826d268e1dd63df1364106066
Christian2323 <christian-wirth@mailbox.org>
2018-01-25 13:28:18 +0100

    UserDaoHibernateImpl teilweise implementiert.

--- server/src/main/java/dao/classes/UserDaoHibernateImpl.java
+++ server/src/main/java/dao/classes/UserDaoHibernateImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.Session;
 import utils.HibernateUtil;
 
 public class UserDaoHibernateImpl implements UserDao{
-    @Override
     public void save(User user) {
         Session session = null;
 
@@ -25,5 +24,42 @@ public class UserDaoHibernateImpl implements UserDao{
         }
     }
 
+    public void delete(User user){
+        Session session = null;
 
+        try{
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            if(user != null){
+                session.delete(user);
+                session.flush();
+            }
+            session.getTransaction().commit();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            HibernateUtil.closeSession(session);
+        }
+    }
+
+    public User getUser(int Id){
+        Session session = null;
+
+        try{
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            User user = (User) session.get(UserDaoHibernateImpl.class, Id);
+            session.getTransaction().commit();
+            return user;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            HibernateUtil.closeSession(session);
+        }
+    }
 }
