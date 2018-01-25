22a7c6e05771e76c09d9e4aa15b12419c4802998
Christian2323 <christian-wirth@mailbox.org>
2018-01-25 15:22:23 +0100

    MessageDaoHibernateImpl: delete() und getMessage() implementiert

--- server/src/main/java/dao/classes/MessageDaoHibernateImpl.java
+++ server/src/main/java/dao/classes/MessageDaoHibernateImpl.java
@@ -24,4 +24,43 @@ public class MessageDaoHibernateImpl implements MessageDao{
             HibernateUtil.closeSession(session);
         }
     }
+
+    public void delete(Message message){
+        Session session = null;
+
+        try{
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            if(message != null){
+                session.delete(message);
+                session.flush();
+            }
+            session.getTransaction().commit();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            HibernateUtil.closeSession(session);
+        }
+    }
+
+    public Message getMessage(int Id){
+        Session session = null;
+
+        try{
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            Message message = (Message) session.get(UserDaoHibernateImpl.class, Id);
+            session.getTransaction().commit();
+            return message;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            HibernateUtil.closeSession(session);
+        }
+    }
 }
