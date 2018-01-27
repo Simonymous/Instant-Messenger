5f4b4e1e8050f47115f3af8e65e460b91adbf106
Simon <simon.triem@yahoo.de>
2018-01-27 14:16:46 +0100

    Util

--- server/src/main/java/utils/HibernateUtil.java
+++ server/src/main/java/utils/HibernateUtil.java
@@ -3,14 +3,20 @@ package utils;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
 
     static {
         try{
-            sessionFactory = new Configuration().configure().buildSessionFactory();
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+            Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+            sessionFactory = metaData.getSessionFactoryBuilder().build();
         }
         catch(Throwable ex){
             System.err.println("Session Factory konnte nicht initialisiert werden" + ex);
