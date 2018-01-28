ea723709cce23d71bcc6bd4f5c1eebb655ca8cf1
Christian Wirth <christian-wirth@mailbox.org>
2018-01-28 17:38:38 +0100

    changeGroupName() erstellt

--- server/src/main/java/dao/classes/Group_UserDaoImpl.java
+++ server/src/main/java/dao/classes/Group_UserDaoImpl.java
@@ -11,13 +11,52 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 import static dao.constants.GlobalConstants.*;
 
+import static dao.constants.GroupDaoConstants.COL_GROUP_ID;
+import static dao.constants.GroupDaoConstants.COL_GROUP_NAME;
 import static dao.constants.Group_UserDaoConstants.*;
+import static dao.constants.UserDaoConstants.*;
 
 public class Group_UserDaoImpl implements Group_UserDao{
+    //------------------------------------------------------------------------------------------------------------------
+// Prepared Statement Begin
+//----------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * INSERT INTO Group_User (userId, GroupId)
+     * VALUES (?,?)
+     */
+    private static final String PS_ADD_NEW_USER_TO_GROUP = "INSERT INTO " + TABLE_GROUP_USERS
+            + "(" + COL_USER_USER_ID + "," + COL_GROUP_GROUP_ID + ") VALUES (?,?)";
+
+    /**
+     * DELETE FROM Group_User
+     * WHERE user_id = ?
+     * AND group_id = ?
+     */
+    private static final String PS_REMOVE_USER_FROM_GROUP = "DELETE FROM " + TABLE_GROUP_USERS + " WHERE " + COL_USER_USER_ID + " = ?"
+            + "AND" + COL_GROUP_GROUP_ID + "=?";
+
+    /**
+     *SELECT * FROM Group_User
+     * WHERE uderId = ?
+     */
+    private static final String PS_GET_GROUPS_BY_USER = "SELECT " + "*" + "FROM" + TABLE_GROUP_USERS + "WHERE" + COL_USER_USER_ID
+            + "=?";
+
+    /**
+     *SELECT * FROM Group_User
+     * WHERE groupId = ?
+     */
+    private static final String PS_GET_USERS_BY_GROUP = "SELECT " + "*" + "FROM" + TABLE_GROUP_USERS + "WHERE" + COL_GROUP_GROUP_ID
+            + "=?";
+
+//----------------------------------------------------------------------------------------------------------------------
+// Prepared Statement End
+//----------------------------------------------------------------------------------------------------------------------
+
     @Override
     public void addNewUser(User aUser, Group aGroup) {
         try (Connection connection = getConnection();
@@ -44,23 +83,22 @@ public class Group_UserDaoImpl implements Group_UserDao{
 
     @Override
     public ArrayList<Group> getGroupsByUser(User aUser) {
-        List<Group> userList = new ArrayList<>();
+        ArrayList<Group> groupList = new ArrayList<>();
+        int id = aUser.getUserId();
         ResultSet rs;
         Group aGroup;
 
         try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(PS_GET_GROUP_FROM_DB)) {
-
+             PreparedStatement statement = connection.prepareStatement(PS_GET_GROUPS_BY_USER)) {
+            statement.setInt(PARAMETER_1, id);
             rs = statement.executeQuery();
 
             while(rs.next()) {
-                aUser = ModelObjectBuilder.getUserObject();
-                aUser.setUsername(rs.getString(COL_USER_USERNAME));
-                aUser.setPassword(rs.getString(COL_USER_PASSWORD));
-                aUser.setUserId(rs.getInt(COL_USER_ID));
-                aUser.setActive(rs.getBoolean(COL_USER_ACTIVE));
+                aGroup = ModelObjectBuilder.getGroupObject();
+                aGroup.setGroupName(rs.getString(COL_GROUP_NAME));
+                aGroup.setGroupId(rs.getInt(COL_GROUP_ID));
 
-                userList.add(aUser);
+                groupList.add(aGroup);
             }
 
         } catch (SQLException e) {
@@ -68,12 +106,37 @@ public class Group_UserDaoImpl implements Group_UserDao{
             e.printStackTrace();
         }
 
-        return userList;
+        return groupList;
     }
 
     @Override
     public ArrayList<User> getUsersByGroup(Group aGroup) {
-        return null;
+        ArrayList<User> userList = new ArrayList<>();
+        int id = aGroup.getGroupId();
+        ResultSet rs;
+        User aUser;
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(PS_GET_USERS_BY_GROUP)) {
+            statement.setInt(PARAMETER_1, id);
+            rs = statement.executeQuery();
+
+            while(rs.next()) {
+                aUser = ModelObjectBuilder.getUserObject();
+                aUser.setUserId(rs.getInt(COL_USER_ID));
+                aUser.setUsername(rs.getString(COL_USER_USERNAME));
+                aUser.setPassword(rs.getString(COL_USER_PASSWORD));
+                aUser.setActive(rs.getBoolean(COL_USER_ACTIVE));
+
+                userList.add(aUser);
+            }
+
+        } catch (SQLException e) {
+            System.err.println(ERR_MSG_GET_USERS_FROM_DB);
+            e.printStackTrace();
+        }
+
+        return userList;
     }
 
     /**
--- server/src/main/java/dao/constants/Group_UserDaoConstants.java
+++ server/src/main/java/dao/constants/Group_UserDaoConstants.java
@@ -4,11 +4,12 @@ import dao.enums.ColNameGroup_User;
 import dao.enums.TableName;
 
 public class Group_UserDaoConstants {
-    public static final String TABLE_GROUP = TableName.Group_User.getTableName();
-    public static final String COL_GROUP_ID = ColNameGroup_User.GroupId.getColumnName();
-    public static final String COL_GROUP_NAME = ColNameGroup_User.UserId.getColumnName();
+    public static final String TABLE_GROUP_USERS = TableName.Group_User.getTableName();
+    public static final String COL_GROUP_GROUP_ID = ColNameGroup_User.GroupId.getColumnName();
+    public static final String COL_USER_USER_ID = ColNameGroup_User.UserId.getColumnName();
 
     public static final String ERR_MSG_ADD_USER_TO_GROUP = "Error: Couldn't add User to Group!\n";
     public static final String ERR_REMOVE_USER_FROM_GROUP = "Error: Couldn't remove User from Group!\n";
     public static final String ERR_MSG_GET_GROUP_FROM_DB = "Error: Group not found!\n";
+    public static final String ERR_MSG_GET_USERS_FROM_DB = "Error: Users not found!\n";
 }
