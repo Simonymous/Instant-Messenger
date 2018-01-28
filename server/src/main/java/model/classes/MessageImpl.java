bd4a0580441d3d9a1171bf3e750c4847115f4a1b
Christian Wirth <christian-wirth@mailbox.org>
2018-01-28 14:32:20 +0100

    Message Model Implementation

--- server/src/main/java/model/classes/MessageImpl.java
+++ server/src/main/java/model/classes/MessageImpl.java
@@ -1,8 +1,61 @@
 package model.classes;
 
+import model.interfaces.Group;
 import model.interfaces.Message;
+import model.interfaces.User;
 
-//TODO Annotationen hinzufügen
-//TODO Implements hinzufügen
-public class MessageImpl {
+//TODO Eventuell Content als eigene Klasse
+public class MessageImpl implements Message {
+    private long messageId;
+    private Group group;
+    private User user;
+    private String content;
+
+    public MessageImpl(Group aGroup, User aUser, String content){
+        this.group = group;
+        this.user = user;
+        this.content = content;
+    }
+
+    @Override
+    public long getMessageId() {
+        return messageId;
+    }
+
+    @Override
+    public void setMessageId(long messageId) {
+        this.messageId = messageId;
+    }
+
+    @Override
+    public Group getGroup() {
+        return group;
+    }
+
+    @Override
+    public void setGroup(Group aGroup) {
+        this.group = group;
+    }
+
+    @Override
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
 }
