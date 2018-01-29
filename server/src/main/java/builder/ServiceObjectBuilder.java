48f4dc6e11620a76237da4e92c2875b34d741910
Christian2323 <christian-wirth@mailbox.org>
2018-01-29 12:33:10 +0100

    ServiceObjectBuilder erstellt

--- server/src/main/java/builder/ServiceObjectBuilder.java
+++ server/src/main/java/builder/ServiceObjectBuilder.java
@@ -1,4 +1,22 @@
 package builder;
 
+import service.classes.GroupServiceImpl;
+import service.classes.MessageServiceImpl;
+import service.classes.UserServiceImpl;
+import service.interfaces.GroupService;
+import service.interfaces.MessageService;
+import service.interfaces.UserService;
+
 public class ServiceObjectBuilder {
+    public static GroupService getGroupServiceObject(){
+        return new GroupServiceImpl();
+    }
+
+    public static MessageService getMessageServiceObject(){
+        return new MessageServiceImpl();
+    }
+
+    public static UserService getUserServiceObject(){
+        return new UserServiceImpl();
+    }
 }
