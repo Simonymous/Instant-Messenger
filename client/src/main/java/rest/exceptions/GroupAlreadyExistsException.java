dc37b3ca9d37bc05ed9a6f3c5b9075fe1433c3d1
QWiiK <nico.enderlein@t-online.de>
2018-03-15 10:36:40 +0100

    added JavaDoc comment

--- client/src/main/java/rest/exceptions/GroupAlreadyExistsException.java
+++ client/src/main/java/rest/exceptions/GroupAlreadyExistsException.java
@@ -2,7 +2,9 @@ package rest.exceptions;
 
 import static rest.constants.GroupRestConstants.ERR_GROUP_ALREADY_EXISTS;
 
-
+/**
+ * own exception to throw if a group already exists
+ */
 public class GroupAlreadyExistsException extends Exception {
     private static final long serialVersionID = 2L;
 
